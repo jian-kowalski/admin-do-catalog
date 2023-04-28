473832434db1702544c04f396db9e98efa4024c0
jian.kowalski <jian.kowalski@sankhya.com.br>
2023-04-28 14:36:58 -0300

    Adicionando comportamento de busca por id da categoria.

---
+++ application/src/main/java/com/jiankowalski/admin/catalogo/application/category/retrieve/get/GetCategoryByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.jiankowalski.admin.catalogo.application.category.retrieve.get;
+
+import com.jiankowalski.admin.catalogo.application.UseCase;
+
+public abstract class GetCategoryByIdUseCase extends UseCase<String, CategoryOutput> {
+}
