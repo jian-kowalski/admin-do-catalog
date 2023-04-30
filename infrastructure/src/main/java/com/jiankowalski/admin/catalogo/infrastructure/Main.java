b39fba11614d79d9e4166796419805d4028ba851
jian.kowalski <jian.kowalski@sankhya.com.br>
2023-04-30 15:53:30 -0300

    Inclusão do flyway ao projeto

--- infrastructure/src/main/java/com/jiankowalski/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/jiankowalski/admin/catalogo/infrastructure/Main.java
@@ -3,10 +3,12 @@ package com.jiankowalski.admin.catalogo.infrastructure;
 import com.jiankowalski.admin.catalogo.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "developement");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
